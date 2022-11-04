767e3f43ef304c3dcc161595c0dec895f268a293
Noname-creat0r <egorchik228322@gmail.com>
2022-11-05 01:41:35 +0300

    0.1.3: Begining to work on the login and signup functionality. Little adjustments.

--- app/src/main/java/com/example/videoservice/auth/activities/LoginActivity.java
+++ app/src/main/java/com/example/videoservice/auth/activities/LoginActivity.java
@@ -3,11 +3,22 @@ package com.example.videoservice.auth.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.videoservice.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_dialog);
+
+        findViewById(R.id.login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //handleLoginDialog();
+            }
+        });
     }
 }
