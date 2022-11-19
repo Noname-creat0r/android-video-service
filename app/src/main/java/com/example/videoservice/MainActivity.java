57c70f745af84e0636ceec0506608289ea67bcb6
Noname-creat0r <egorchik228322@gmail.com>
2022-11-19 18:13:42 +0300

    0.2.0: DrawerLayout was added to the main form.

--- app/src/main/java/com/example/videoservice/MainActivity.java
+++ app/src/main/java/com/example/videoservice/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.videoservice;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -23,16 +26,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
 
-        this.findViewById(R.id.avatar).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.userIconMenu).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 createAuthDialog();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     private void createAuthDialog(){
